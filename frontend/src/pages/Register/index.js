4ab30dadb6f5c4109344f5e2bab7da3a4c3b320e
Your Name <rafael.academianow@gmail.com>
2020-03-27 22:52:48 -0300

    ss

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+
+export default function Register(){
+    return (
+    <div className="register-container">
+        <div className="content">
+            <section>
+<img src={logoImg} alt="Be the Hero"/>
+
+<h1>Cadastro</h1>
+<p>faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+            
+<Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041"/>
+                        Nao tenho cadastro
+                    </Link>
+            
+            </section>
+            <form>
+                <input placeholder="Nome da ONG"/>
+                <input type="email" placeholder="E-mail" />
+                <input placeholder="whatsapp" />
+
+                <div className="input-group">
+                    <input placeholder="cidade" />
+                    <input placeholder="uf" style={{eidth: 80}} />
+                </div>
+
+                <button className="button" type="submit">Cadastrar</button>
+            </form>
+        </div>
+
+    </div>
+
+        );
+}
